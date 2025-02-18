d5502e6b16842c65bcf1023948c575153b32ce09
papercontroller <sanjarmaulkan2102@mail.ru>
2025-02-18 22:22:54 +0500

    upd

--- cmd/main.go
+++ cmd/main.go
@@ -28,13 +28,34 @@ func main() {
 		log.Fatal(err)
 		return
 	}
+	menu := &telebot.ReplyMarkup{ResizeKeyboard: true}
+	btnStart := menu.Text("Go quiz")
 
-	b.Handle("/hello", func(c telebot.Context) error {
-		return c.Send("Hello!")
+	options := &telebot.ReplyMarkup{ResizeKeyboard: true}
+	btnFirst := options.Text("1")
+	btnSecond := options.Text("2")
+	btnThird := options.Text("3")
+	btnFourth := options.Text("4")
+
+	menu.Reply(
+		menu.Row(btnStart),
+	)
+
+	options.Reply(
+		menu.Row(btnFirst, btnSecond, btnThird, btnFourth),
+	)
+
+	b.Handle("/start", func(c telebot.Context) error {
+
+		return c.Send("Hello! This is Quiz Bot! Do you want to test your knowledge?", menu)
 	})
 
-	b.Handle("/info", func(c telebot.Context) error {
-		return c.Send("This is quiz bot")
+	b.Handle(&btnStart, func(c telebot.Context) error {
+		c.Send("Lets GO")
+
+		question := "first question"
+
+		return c.Send(question, options)
 	})
 
 	b.Start()
